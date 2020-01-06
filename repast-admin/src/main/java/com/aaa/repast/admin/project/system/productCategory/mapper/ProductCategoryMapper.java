dbe4443ff8985f888cd9e48f9793f1785751b385
liuyang1209 <58672331+liuyang1209@users.noreply.github.com>
2020-01-06 16:55:42 +0800

    刘扬：商品分类

--- repast-admin/src/main/java/com/aaa/repast/admin/project/system/productCategory/mapper/ProductCategoryMapper.java
+++ repast-admin/src/main/java/com/aaa/repast/admin/project/system/productCategory/mapper/ProductCategoryMapper.java
@@ -1,7 +1,10 @@
 package com.aaa.repast.admin.project.system.productCategory.mapper;
 
 import com.aaa.repast.admin.project.system.productCategory.domain.ProductCategory;
-import java.util.List;	
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * 产品分类 数据层
@@ -9,6 +12,7 @@ import java.util.List;
  * @author Seven Lee
  * @date 2019-12-28
  */
+@Repository
 public interface ProductCategoryMapper 
 {
 	/**
@@ -58,5 +62,65 @@ public interface ProductCategoryMapper
      * @return 结果
      */
 	public int deleteProductCategoryByIds(String[] ids);
-	
+
+
+	// 新增的方法
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/2 21:15
+	 * @Description
+	 * 添加一级类目 并返回对应主键id
+	 */
+	public Long returnId(ProductCategory productCategory);
+
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/2 21:37
+	 * @Description
+	 * 根据类目id 添加或修改 图片路径
+	 */
+	Integer getIcon(@Param("icon") String icon, @Param("id") Long id);
+
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/2 20:40
+	 * @Description
+	 * 新增保存产品分类
+	 */
+	public List<ProductCategory> selectTow(Long id);
+
+
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/3 17:05
+	 * @Description
+	 * 修改二级类目的信息(不修改图片)
+	 */
+	public Integer updateTwoInfo(ProductCategory productCategory);
+
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/3 17:12
+	 * @Description
+	 * 修改 图片的路径
+	 */
+	public Integer updateTwoInfoAndImage(ProductCategory productCategory);
+
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/3 19:35
+	 * @Description
+	 * 删除（修改状态）一级类目
+	 */
+	public Integer delTwoStatus(Long id);
+
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/6 09:30
+	 * @Description
+	 * 	添加二级类目
+	 */
+
+	  public  Integer twoProinfo(ProductCategory productCategory);
+
 }
--- repast-admin/src/main/java/com/aaa/repast/admin/project/system/productCategory/service/ProductCategoryServiceImpl.java
+++ repast-admin/src/main/java/com/aaa/repast/admin/project/system/productCategory/service/ProductCategoryServiceImpl.java
@@ -1,11 +1,18 @@
 package com.aaa.repast.admin.project.system.productCategory.service;
 
+import com.aaa.repast.admin.framework.ftp.FileNameUtil;
+import com.aaa.repast.admin.framework.ftp.FtpProperties;
+import com.aaa.repast.admin.framework.ftp.FtpUtil;
 import com.aaa.repast.common.support.Convert;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Random;
+
 import com.aaa.repast.admin.project.system.productCategory.mapper.ProductCategoryMapper;
 import com.aaa.repast.admin.project.system.productCategory.domain.ProductCategory;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 产品分类 服务层实现
@@ -78,5 +85,194 @@ public class ProductCategoryServiceImpl implements IProductCategoryService
 	{
 		return productCategoryMapper.deleteProductCategoryByIds(Convert.toStrArray(ids));
 	}
-	
+	//添加的方法
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/2 21:03
+	 * @Description
+	 * 添加一级类目
+	 */
+	@Override
+	public Boolean addProductCategory(ProductCategory productCategory, MultipartFile file) {
+		FtpProperties ftp = new FtpProperties();
+		// 如果id为null 或 "" 说明是添加
+		if(productCategory.getId()==null || "".equals(productCategory.getId())){
+
+			// 返回一级类目的主键
+			Long id = productCategoryMapper.returnId(productCategory);
+			if(id==null){
+				return  false;
+			}
+			try {
+				//获取上传图片的名字
+				String oldName = file.getOriginalFilename();
+				//生成新的文件名
+				String newName= FileNameUtil.getFileName(Long.valueOf(productCategory.getId()));
+				//截取原始文件名的后缀,拼接到新的名称上--->获取新的文件名
+				newName = newName + oldName.substring(oldName.lastIndexOf("."));
+				//获取文件的路径(2019/11/13)
+				String filePath = new DateTime().toString("yyyy/MM/dd");
+				//调用上传文件的工具类
+				boolean ifSuccess =	FtpUtil.uploadFile(ftp.getIpAddr(),ftp.getPort(),ftp.getUsername(),ftp.getPassword(),ftp.getBasePath(),filePath,newName
+						,file.getInputStream());
+				if(ifSuccess){
+					String headPic= ftp.getHttpPath()+"/"+filePath+"/"+newName;
+					Integer result = productCategoryMapper.getIcon(headPic, productCategory.getId());
+					if(result>0){
+						return true;
+					}
+				}else {
+					return  false;
+				}
+			}catch (Exception e){
+
+			}
+
+		}else{
+			// 否则为修改
+			//获取上传图片的名字
+			String oldName = file.getOriginalFilename();
+			if("".equals(oldName) || null==oldName ){
+				Integer result = productCategoryMapper.updateTwoInfo(productCategory);
+				if(result>0){
+					return true;
+				}
+			}else {
+				try {
+					//生成新的文件名
+					String newName= FileNameUtil.getFileName(Long.valueOf(productCategory.getId()));
+					//截取原始文件名的后缀,拼接到新的名称上--->获取新的文件名
+					newName = newName + oldName.substring(oldName.lastIndexOf("."));
+					//获取文件的路径(2019/11/13)
+					String filePath = new DateTime().toString("yyyy/MM/dd");
+					//调用上传文件的工具类
+					boolean ifSuccess =	FtpUtil.uploadFile(ftp.getIpAddr(),ftp.getPort(),ftp.getUsername(),ftp.getPassword(),ftp.getBasePath(),filePath,newName
+							,file.getInputStream());
+
+					if(ifSuccess){
+						String headPic= ftp.getHttpPath()+"/"+filePath+"/"+newName;
+						productCategory.setIcon(headPic);
+						Integer integer = productCategoryMapper.updateTwoInfoAndImage(productCategory);
+						if(integer>0){
+							return true;
+						}
+					}
+				}catch (Exception e){
+
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/2 20:40
+	 * @Description
+	 * 根据一级类目的id 查询二级类目的商品信息
+	 */
+	@Override
+	public List<ProductCategory> selectTow(Long id) {
+		List<ProductCategory> p = productCategoryMapper.selectTow(id);
+		if(null !=p){
+			return  p;
+		}
+		return null;
+	}
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/3 18:52
+	 * @Description
+	 *  修改二级类目信息
+	 */
+	@Override
+	public Boolean twoInfo(ProductCategory productCategory, MultipartFile file) {
+		FtpProperties ftp = new FtpProperties();
+		//获取上传图片的名字
+		String oldName = file.getOriginalFilename();
+		if("".equals(oldName) || null==oldName ){
+			Integer result = productCategoryMapper.updateTwoInfo(productCategory);
+			if(result>0){
+				return true;
+			}
+		}else{
+			try {
+				//生成新的文件名
+				String newName= FileNameUtil.getFileName(Long.valueOf(productCategory.getId()));
+				//截取原始文件名的后缀,拼接到新的名称上--->获取新的文件名
+				newName = newName + oldName.substring(oldName.lastIndexOf("."));
+				//获取文件的路径(2019/11/13)
+				String filePath = new DateTime().toString("yyyy/MM/dd");
+				//调用上传文件的工具类
+				boolean ifSuccess =	FtpUtil.uploadFile(ftp.getIpAddr(),ftp.getPort(),ftp.getUsername(),ftp.getPassword(),ftp.getBasePath(),filePath,newName
+						,file.getInputStream());
+
+				if(ifSuccess){
+					String headPic= ftp.getHttpPath()+"/"+filePath+"/"+newName;
+					productCategory.setIcon(headPic);
+					Integer integer = productCategoryMapper.updateTwoInfoAndImage(productCategory);
+					if(integer>0){
+						return true;
+					}
+				}
+			}catch (Exception e){
+
+			}
+		}
+		return false;
+	}
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/3 19:35
+	 * @Description
+	 * 删除（修改状态）一级类目
+	 */
+	@Override
+	public Boolean delTwoStatus(Long id) {
+		Integer result = productCategoryMapper.delTwoStatus(id);
+		if(result>0){
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @Author Yang
+	 * @Date Create in  2020/1/6 09:45
+	 * @Description
+	 *  添加二级类目
+	 */
+	@Override
+	public Boolean twoProinfo(ProductCategory productCategory,MultipartFile file) {
+		FtpProperties ftp = new FtpProperties();
+		//获取上传图片的名字
+		String oldName = file.getOriginalFilename();
+		Random r = new Random();
+		int i = r.nextInt(1000);
+		try {
+		//生成新的文件名
+		String newName= FileNameUtil.getFileName(Long.valueOf(i));
+		//截取原始文件名的后缀,拼接到新的名称上--->获取新的文件名
+		newName = newName + oldName.substring(oldName.lastIndexOf("."));
+		//获取文件的路径(2019/11/13)
+		String filePath = new DateTime().toString("yyyy/MM/dd");
+		//调用上传文件的工具类
+		boolean ifSuccess =	FtpUtil.uploadFile(ftp.getIpAddr(),ftp.getPort(),ftp.getUsername(),ftp.getPassword(),ftp.getBasePath(),filePath,newName
+				,file.getInputStream());
+
+		if(ifSuccess){
+			String headPic= ftp.getHttpPath()+"/"+filePath+"/"+newName;
+			productCategory.setIcon(headPic);
+			Integer integer = productCategoryMapper.twoProinfo(productCategory);
+			if(integer>0){
+				return true;
+			}
+		}
+	}catch (Exception e){
+
+}
+		return false;
+	}
+
 }
